6438d432bb692a9e7cc42acf90f61ac7a0330a89
Jessie-08 <74401923+Jessie-08@users.noreply.github.com>
2021-05-10 09:18:22 +0530

    Update block.js

--- Project-Checking-main/Project 29/block.js
+++ Project-Checking-main/Project 29/block.js
@@ -1,7 +1,7 @@
 class Block{
     constructor(x, y, width, height) {
         var options = {
-            density:0.0003           
+              
         }
         this.body = Bodies.rectangle(x, y, width, height, options);
         this.width = width;
